c0f19d57bc802cb9d7d7d323ed080fff9f6b3e55
Rashmi-Wijesekara <rashmiwijesekara1999@gmail.com>
2022-10-10 16:05:26 +0530

    all projects added

--- client/src/portfolio/containers/Projects.js
+++ client/src/portfolio/containers/Projects.js
@@ -6,14 +6,22 @@ const Projects = () => {
 	const projectsData = projects.projects
 
 	return (
-		<div className="flex flex-wrap flex-row gap-5 md:mx-20">
-			{
-				projectsData.map((project) => (
+		<div className="my-40">
+			<div className="my-10">
+				<div className="font-poppins text-center md:text-lg font-semibold text-fontLightBlue tracking-widest">
+					MY WORKS
+				</div>
+				<div className="font-poppins text-center text-xl md:text-3xl font-semibold text-white md:mt-5">
+					LATEST PROJECTS
+				</div>
+			</div>
+			<div className="flex flex-wrap flex-row gap-5 md:mx-20">
+				{projectsData.map((project) => (
 					<ProjectCard key={project.id} project={project} />
-				))
-			}
+				))}
+			</div>
 		</div>
-	)
+	);
 }
 
 export default Projects
